35052bbf2c8dd3e6ebc933bce00a55c3ce58ddbd
kunalSHU <kunalshukla@hotmail.com>
2025-06-16 14:40:17 -0400

    Added back test endpoint, temporary

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const vehicleRoutes = require('./routes/vehicleRoutes');
 
 const app = express();
 const port = process.env.PORT || 4201;
+const router = express.Router();
 
 app.use(cors());
 app.use(express.json()); // Allows for POST/PUT parsing
@@ -13,9 +14,16 @@ app.use(express.json()); // Allows for POST/PUT parsing
 app.use('/api/services', servicesRoutes);         // Handles GET /api/services/list
 app.use('/api/vehicle', vehicleRoutes);           // Handles GET /api/vehicle/info
 
+app.use('/api', router)
+
 // 404 handler
 app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
+});
+
+// For testing, temporary
+router.get('/test', (req, res) => {
+    res.json({message: "Hello World"});
 });
 
 app.listen(port, () => {
